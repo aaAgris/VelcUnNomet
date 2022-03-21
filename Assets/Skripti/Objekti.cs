f891061b674de02f525e37e4f8ccf30370470ee0
aaAgris <antansagris@gmail.com>
2022-03-21 06:35:46 -0700

    masinas vecotr

--- Assets/Skripti/Objekti.cs
+++ Assets/Skripti/Objekti.cs
@@ -8,6 +8,15 @@ public class Objekti : MonoBehaviour {
 	public GameObject atkritumuMasina;
 	public GameObject atraPalidziba;
 	public GameObject autobuss;
+	public GameObject policija;
+	public GameObject cementamas;
+	public GameObject ugunsdzesejs;
+	public GameObject TraktorsSej;
+	public GameObject traktors;
+	public GameObject ekskavators;
+	public GameObject bembis;
+	public GameObject b2;
+
 
 	//uzglabaa objekta sakitnejkas vietas koordinatas
 	[HideInInspector]
@@ -16,6 +25,22 @@ public class Objekti : MonoBehaviour {
 	public Vector2 atroKoord;
 	[HideInInspector]
 	public Vector2 bussKoord;
+	[HideInInspector]
+	public Vector2 policjaKoord;
+	[HideInInspector]
+	public Vector2 cementaKoord;
+	[HideInInspector]
+	public Vector2 ugunsKoord;
+	[HideInInspector]
+	public Vector2 TraktorsSejKoord;
+	[HideInInspector]
+	public Vector2 TraktorsKoord;
+	[HideInInspector]
+	public Vector2 ekskavatorsKoord;
+	[HideInInspector]
+	public Vector2 bembisKoord;
+	[HideInInspector]
+	public Vector2 b2Koord;
 
 	//uzglabas audi avotu kur bus attelu skanas efekti
 	public AudioSource skanasAvots;
@@ -30,6 +55,16 @@ public class Objekti : MonoBehaviour {
 		atroKoord = atkritumuMasina.GetComponent<RectTransform> ().localPosition;
 		atroKoord = atraPalidziba.GetComponent<RectTransform> ().localPosition;
 		bussKoord = autobuss.GetComponent<RectTransform> ().localPosition;
+		policjaKoord = policija.GetComponent<RectTransform> ().localPosition;
+		cementaKoord = cementamas.GetComponent<RectTransform> ().localPosition;
+		ugunsKoord = ugunsdzesejs.GetComponent<RectTransform> ().localPosition;
+		TraktorsSejKoord = TraktorsSej.GetComponent<RectTransform> ().localPosition;
+		TraktorsKoord = traktors.GetComponent<RectTransform> ().localPosition;
+		ekskavatorsKoord = ekskavators.GetComponent<RectTransform> ().localPosition;
+		bembisKoord = bembis.GetComponent<RectTransform> ().localPosition;
+		b2Koord = b2.GetComponent<RectTransform> ().localPosition;
+
+
 	}
 	
 	// Update is called once per frame
